ffa7e84b7a4da389e2a6c5e8d7f213f004448a1c
Soumik Maity <m.027soumik@gmail.com>
2026-04-08 21:04:41 +0530

    Add session-based tasks: device-specific data

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 # Create your views here.
+
+# Helper function to get or create session key
+def get_session_key(request):
+    if not request.session.session_key:
+        request.session.create()
+    return request.session.session_key
+
+# Home page - list tasks for this session only
+def home(request):
+    session_key = get_session_key(request)
+    tasks = Task.objects.filter(session_key=session_key)
+
+    if request.method == "POST":
+        task_text = request.POST.get('task')
+        if task_text:
+            Task.objects.create(task=task_text, session_key=session_key)
+        return redirect('home')
+
+    context = {'tasks': tasks}
+    return render(request, 'home.html', context)
+
+# add tasks
 def addTask(request):
     task=(request.POST['task'])
     Task.objects.create(task=task)
@@ -40,3 +62,4 @@ def delete_task(request, pk):  # <-- It must be pk!
     task = get_object_or_404(Task, id=pk)
     task.delete()
     return redirect('home')
+
